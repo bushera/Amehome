310e7ebffc6744485fe0ee59a612138305a3814e
bushera <59298086+bushera@users.noreply.github.com>
2020-10-19 14:42:38 +0100

    update

--- topfloor.js
+++ topfloor.js
@@ -5,6 +5,19 @@ $(function(){
 		$("#preload-page").delay(50).fadeOut(800);
 		});
 		
+		
+		$("#subscribe").click(function(){
+			$("#subscribe-cover").css("display", "block");	
+		});
+		
+		$("#cancel1").click(function(){
+			$("#subscribe-cover").css("display", "none");	
+		});
+		
+		$("#email-sub0").keydown(function(){
+			$("#btn-sub0").css("display", "block");
+		});
+		
   
 	});
 
