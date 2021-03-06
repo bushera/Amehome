9f70199d436831694aa75d02bf1ccc7c62eb54d8
bushera <59298086+bushera@users.noreply.github.com>
2021-03-06 22:42:17 +0100

    yes

--- topfloor.js
+++ topfloor.js
@@ -22,6 +22,8 @@ $(function(){
 		$("#showbar").animate({left: "-100%"}, 700);
 		$("#menu-btn").css("display", "none");
 		$("#offslide-h1").animate({left: "15%"} ,900);
+		$("#offslide-down-1").animate({bottom: "0%"} ,200);
+		$("#offslide-down-1").animate({width: "100%"} ,400);
 		$("#menu-btn").off(click);
 	});
 
@@ -32,6 +34,8 @@ $(function(){
 		$("#showbar").animate({left: "0%"}, 500);
 		$("#offslide-h1").animate({left: "-70%"}, 900);
 		$("#close-trigger").css("display", "block");
+		$("#offslide-down-1").animate({bottom: "-108%"} ,400);
+		$("#offslide-down-1").animate({width: "0%"} ,300);
 		$("#close-trigger").off(click);
 	});
 
